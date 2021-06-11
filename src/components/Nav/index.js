1df2fe127b490b026e48b1ab59e6b6589596225c
Maria M <prov21_1@hotmail.com>
2021-06-11 12:23:21 -0700

    new nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,12 +1,47 @@
-//import React, { useEffect } from "react";
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
 
 function Nav() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab label="About" />
+        <Tab label="My work" />
+        <Tab label="Resume" />
+        <Tab label="Contact" />
+      </Tabs>
+    </Paper>
+  );
+}
+
+/* function Nav() {
   //change title of browser tab
   /* useEffect(() => {
     document.title = currentCategory.name;
   }, [currentCategory]);
- */
+ 
   return (
     <header className="flex-row px-1">
       <h2>
@@ -36,5 +71,5 @@ function Nav() {
     </header>
   );
 }
-
+*/
 export default Nav;
