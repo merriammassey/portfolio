225721860d2876a19d184743ba37e40457d1c636
Maria M <prov21_1@hotmail.com>
2021-07-09 11:01:59 -0700

    update nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,10 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, Container, Modal, Tab } from "react-bootstrap";
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import { Link } from "react-router-dom";
 import "./style.css";
