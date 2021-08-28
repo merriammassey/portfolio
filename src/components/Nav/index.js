e15ff999738132c28dad89acd2a48ee6bcefbbc9
Maria M <prov21_1@hotmail.com>
2021-08-28 13:01:07 -0700

    updated resume

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -17,7 +17,7 @@ const useStyles = makeStyles({
 
 function AppNav() {
   const classes = useStyles();
-  const [value, setValue] = React.(0);
+  const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
